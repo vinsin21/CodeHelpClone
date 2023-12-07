14c8b7aada8aae94a24f73adc81fddebb5ce5373
vinsin21 <vineet.singh.jrj@gmail.com>
2023-12-07 16:11:52 +0530

    added loader animation at start of website

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+
+let tl = gsap.timeline()
+
+tl.from(".loader>img", {
+    scale: 0,
+    duration: 0.8,
+    opacity: 0
+})
+
+tl.to(".loader>img", {
+    scale: 6,
+    duration: 0.8,
+    opacity: 0.2,
+    filter: "blur(5px)"
+})
+
+
+tl.to(".loader", {
+    // transform: "translateY(-100vh)"
+    opacity: 0
+
+})
+
+
+
+
+
+
+
 gsap.to(" .page7 .page7-card   ", {
     transform: "translateX(-200%)",
     scrollTrigger: {
